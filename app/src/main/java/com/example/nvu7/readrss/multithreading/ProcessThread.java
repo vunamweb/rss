dd265c07660fc55b1cf9d538d22d53685ce81126
vu <nam>
2018-05-30 23:16:08 +0700

    add assynctask

--- app/src/main/java/com/example/nvu7/readrss/multithreading/ProcessThread.java
+++ app/src/main/java/com/example/nvu7/readrss/multithreading/ProcessThread.java
@@ -1,11 +1,16 @@
 package com.example.nvu7.readrss.multithreading;
 
+import android.os.Message;
+
+import com.example.nvu7.readrss.common.Constants;
 import com.example.nvu7.readrss.model.Rss;
 import com.example.nvu7.readrss.module.RssService;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 /**
  * Created by nvu7 on 5/24/2018.
@@ -13,9 +18,11 @@ import java.util.List;
 
 public class ProcessThread extends Thread {
     private List<Rss> items;
-    public ProcessThread(List<Rss> items)
+    private android.os.Handler handler;
+    public ProcessThread(List<Rss> items, android.os.Handler handler)
     {
         this.items=items;
+        this.handler=handler;
     }
     @Override
     public void run() {
@@ -62,6 +69,11 @@ public class ProcessThread extends Thread {
                         description = null;
                         isItem = false;
                     }
+                }
+                if(Constants.USE_HANDLER)
+                {
+                    Message msg = handler.obtainMessage(1, (ArrayList<Rss>) items);
+                    handler.sendMessage(msg);
                 }
             }
 
